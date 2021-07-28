ea2c00b7f2222e9299ff033cf0c22b2f70dff8ef
Kori Reine <kkr.reine@gmail.com>
2021-07-27 21:21:21 -0500

    Add pretty testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'html_page#home'
   get 'html_page/help'
   get 'html_page/about'
+  get 'html_page/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
