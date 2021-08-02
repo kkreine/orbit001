82e522f7eb7a2ddcba7cf5b3ddab0503c512fd88
Kori Reine <kkr.reine@gmail.com>
2021-08-02 15:47:07 -0500

    Add login and authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Login the user
+      log_in user
+      redirect_to user
+    else
+      # Create error message
+      flash.now[:danger] = "Invalid email or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/about', to: 'html_page#about'
   get '/contact', to: 'html_page#contact'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
